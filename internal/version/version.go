f90ce9e6bdbd4c9a8168ab8c9adf79b708bf7c0e
OCGT Developer <dev@ocgt.local>
2026-06-18 13:12:09 +0800

    [release] v2.1.0 — Hub 跨设备同步功能

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,4 +3,4 @@ package version
 // Version is the current version of ocgt
 // This value can be overridden at build time using -ldflags:
 //   go build -ldflags "-X github.com/ethan-blue/open-code-go-tools/internal/version.Version=0.2.1"
-var Version = "2.0.5"
+var Version = "2.1.0"
